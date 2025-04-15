a45d2b49dc3e3c34741f82a415618be0ded2ba69
funmi <maryamusan77@gmail.com>
2025-04-15 05:08:47 +0100

    add custom colors to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    
-      content: ["./app/**/*.{js,ts,tsx}", "./components/**/*.{js,ts,tsx}"],
-    
-    presets: [require("nativewind/preset")],
-    theme: {
-      extend: {},
+  content: ['./app/**/*.{js,ts,tsx}', './components/**/*.{js,ts,tsx}'],
+
+  presets: [require('nativewind/preset')],
+  theme: {
+    extend: {
+      colors: {
+        // Add your custom colors here
+        primary: '#FFB0FE',
+        body: '#FFFFFF',
+        grey: '#8A8CA9',
+        mint: '#7ED3B2'
+        // 'accent': '#F59E0B',
+        // 'danger': '#EF4444',
+        // // You can also add variants
+        // 'primary-light': '#93C5FD',
+        // 'primary-dark': '#1E40AF',
+      },
     },
-    plugins: [],
-  }
+  },
+  plugins: [],
+};
