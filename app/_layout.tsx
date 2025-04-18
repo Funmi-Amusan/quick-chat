8570b17c6a3b5c8fe8a49f6bf77180e883c9ada5
funmi <maryamusan77@gmail.com>
2025-04-18 13:06:05 +0100

    install tanstack query

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,25 +1,23 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { SessionProvider } from 'context/authContext';
 import { Slot } from 'expo-router';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { persistor, store, useAppSelector } from '../store';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
 
 import '../global.css';
 import { NotificationProvider } from '~/context/NotificationContext';
 
+const queryClient = new QueryClient();
+
 export default function Root() {
   return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <NotificationProvider>
-          <SessionProvider>
-            <GestureHandlerRootView style={{ flex: 1 }}>
-              <Slot />
-            </GestureHandlerRootView>
-          </SessionProvider>
-        </NotificationProvider>
-      </PersistGate>
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <NotificationProvider>
+        <SessionProvider>
+          <GestureHandlerRootView style={{ flex: 1 }}>
+            <Slot />
+          </GestureHandlerRootView>
+        </SessionProvider>
+      </NotificationProvider>
+    </QueryClientProvider>
   );
 }
