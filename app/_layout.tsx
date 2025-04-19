6a62fe3b5c262bbd29af22fecb08226994b02724
funmi <maryamusan77@gmail.com>
2025-04-19 19:26:29 +0100

    add toast messages

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,9 +2,11 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { SessionProvider } from 'context/authContext';
 import { Slot } from 'expo-router';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import Toast from 'react-native-toast-message';
 
 import '../global.css';
 import { NotificationProvider } from '~/context/NotificationContext';
+import toastConfig from '~/lib/toast-config';
 
 const queryClient = new QueryClient();
 
@@ -16,6 +18,7 @@ export default function Root() {
           <GestureHandlerRootView style={{ flex: 1 }}>
             <Slot />
           </GestureHandlerRootView>
+          <Toast config={toastConfig} visibilityTime={5000} />
         </SessionProvider>
       </NotificationProvider>
     </QueryClientProvider>
